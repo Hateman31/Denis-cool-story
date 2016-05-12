798d4bd3f8af5ad307e2b1d7fbe543dd65d899d1
hateman31 <metilogrunge31@gmail.com>
2016-05-12 12:23:18 +0300

    another

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ from kivy.clock import Clock
 class Game(BoxLayout):
 	def update(self,dt):
 		self.background.pos[0] -= 0.5
-		
-		if self.background.pos[0]<=0:
+		print(self.background.pos)
+		if self.background.right<=0:
 			self.background.pos[0] = self.width
 	
 class GameApp(App):
 	def build(self):
 		game = Game()
-		Clock.schedule_interval(game.update, 2)
+		Clock.schedule_interval(game.update, 1/50)
 		return game
 
 
