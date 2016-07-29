17af9ddd96b740e5826b4038a7eb7be5a1791742
Hateman31 <metilogrunge31@gmail.com>
2016-07-29 12:21:54 +0300

    made non-stop scrolling

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import kivy
 kivy.require('1.9.1')
 
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.clock import Clock
-from kivy.graphics import Translate, PushMatrix, PopMatrix
+from kivy.graphics import Translate
 
 from player import Player
 from talking import Talking
@@ -25,29 +22,46 @@ class Game(Widget):
 		self.street1 = Background(
 			view_h= Window.height,
 			source = 'images/street1.png'
-			)
+		)
 		
 		self.street2 = Background(
 			view_h= Window.height,
 			source = 'images/street1.png',
 			x = self.street1.right			
-			)
+		)
 			
 		self.add_widget(self.street1)
 		self.add_widget(self.street2)
 		self.add_widget(self.talking)
 		self.add_widget(self.player)
 		
-		#~ self.current = 
+		self.stopline = self.street1.right
 		
 	def update(self,dt):
 		
-		#~ if self.player.right < 1264:
-		if 1:
+		if self.player.right < self.stopline:
 			self.player.update()
 			self.talking.update(self.player)
-			#~ if self.player.x>self.street2.x:
-				#~ self.street.x += self.street.width*2
+		
+		if self.talking.phrase:
+
+			delta = Window.width//2			
+			delta += self.player.right
+			
+			#~ frame's right board == camera right board
+			if delta >= self.street1.right:
+				self.street2.x = self.street1.right
+				self.stopline = self.street2.right
+			else:
+				self.stopline = self.street1.right
+				
+			#~ frame's right board == camera right board
+			if delta >= self.street2.right:
+				self.street1.x = self.street2.right
+				self.stopline = self.street1.right
+			else:
+				self.stopline = self.street2.right
+			
 		self.set_focus(*self.player.center)
 		
 	def set_focus(self, x, y):
@@ -55,19 +69,17 @@ class Game(Widget):
 		self.set_view()
 			
 	def area_set_focus(self,x,y):
-		#~ if self.finish:
-			#~ offset_max_x = self.finish - self.width
-		#~ else:
-			#~ offset_max_x = None
-		offset_max_x = 1264 - self.width
+		offset_max_x = None
+		if not self.talking.phrase:
+			offset_max_x = self.stopline - self.width
 		offset_max_y = self.player.height*1.5
 		offset_min_x = offset_min_y = 0
 		
 		fx = x - self.width//2
 		fy = y - offset_max_y
 		
-		#~ if offset_max_x and (fx > offset_max_x):
-		    #~ fx = offset_max_x
+		if offset_max_x and (fx > offset_max_x):
+		    fx = offset_max_x
 		if fx < offset_min_x:
 		    fx = offset_min_x
 		
